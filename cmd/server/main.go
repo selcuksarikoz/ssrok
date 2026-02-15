0ac9a270f6fe55feb9d0faa88ee9537d13b35093
selcuksarikoz <benimpostahesabim@gmail.com>
2026-02-15 19:15:46 +0100

    fix: added cors

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -173,6 +173,24 @@ func (w *gzipResponseWriter) Flush() {
 	}
 }
 
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		}
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func gzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") ||
@@ -233,6 +251,7 @@ func main() {
 
 	var handler http.Handler = mux
 	handler = recoveryMiddleware(handler)
+	handler = corsMiddleware(handler)
 	handler = security.SecurityHeaders(handler)
 	handler = gzipMiddleware(handler)
 
